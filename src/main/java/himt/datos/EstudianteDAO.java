0ee6fa285c3ab8369110225d34fca42a34859629
HIMT <himt823@gmail.com>
2023-10-17 19:51:28 -0300

    Se añade metodo buscarEstudiantesPorId

--- src/main/java/himt/datos/EstudianteDAO.java
+++ src/main/java/himt/datos/EstudianteDAO.java
@@ -42,11 +42,51 @@ public class EstudianteDAO {
         return estudiantes;
     }
 
+    public boolean buscarEstudiantePorId(Estudiante estudiante){
+        PreparedStatement ps;
+        ResultSet rs;
+        Connection con = getConexion();
+        String sql = "SELECT * FROm estudiante WHERE id_estudiante = ? ";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, estudiante.getIdEstudiante());
+            rs = ps.executeQuery();
+            if(rs.next()){
+                estudiante.setNombre(rs.getString("nombre"));
+                estudiante.setApellido(rs.getString("apellido"));
+                estudiante.setTelefono(rs.getString("telefono"));
+                estudiante.setEmail(rs.getString("email"));
+                return true;
+            }
+        }catch (Exception e){
+            System.out.println("Ocurrio un error al buscar estudiante: " + e.getMessage());
+        }
+        finally {
+            try {
+                con.close();
+            }catch (Exception e){
+                System.out.println("Ocurrio un error al cerrar conexión: " + e.getMessage());
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         var estudoanteDao = new EstudianteDAO();
         // Listar estudiantes
-        System.out.println("Listado estudiantes: ");
+        //System.out.println("Listado estudiantes: ");
         List<Estudiante> estudiantes = estudoanteDao.listarEstudiantes();
-        estudiantes.forEach(System.out::println);
+        //estudiantes.forEach(System.out::println);
+
+        //Buscar por Id
+        var estudiante1 = new Estudiante(1);
+        System.out.println("Estudiante antes de la busqueda: "+ estudiante1);
+        var encontrado = estudoanteDao.buscarEstudiantePorId(estudiante1);
+        if(encontrado){
+            System.out.println("Estudiante encontrado: " + estudiante1);
+        }
+        else{
+            System.out.println("No se encontró estudiante: " + estudiante1.getIdEstudiante());
+        }
     }
 }
